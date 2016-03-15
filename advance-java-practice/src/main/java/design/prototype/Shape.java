f2cb473d41a848b1e5fd2b9473e8accf25c8a487
omprakash.yadav <omprakash.yadav@flipkart.com>
2016-03-15 09:56:04 +0530

    added strategy and tamplate pattern design samples to advance java

--- advance-java-practice/src/main/java/design/prototype/Shape.java
+++ advance-java-practice/src/main/java/design/prototype/Shape.java
@@ -3,6 +3,9 @@ package design.prototype;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+
 /**
  * Created by omprakash.yadav on 13/03/16.
  */
@@ -14,7 +17,7 @@ public abstract class Shape implements Cloneable {
     protected String type;
 
     public abstract void draw();
-
+//    ObjectInputStream in=new ObjectInputStream(new FileInputStream("f.txt"));
     @Override
     public Object clone() {
         Object clone = null;
---
+++ advance-java-practice/src/main/java/design/strategy/Strategy.java
@@ -0,0 +1,8 @@
+package design.strategy;
+
+/**
+ * Created by omprakash.yadav on 15/03/16.
+ */
+public interface Strategy {
+    public int doOperation(int num1, int num2);
+}
